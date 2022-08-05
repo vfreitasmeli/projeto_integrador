ac48e2b699d260039adaa7d9d8c109615c316ffa
brunavottri <bruna.vottri@mercadolivre.com>
2022-08-05 16:46:07 -0300

    feat: create import.sql

--- seed.sql
+++
@@ -1,7 +0,0 @@
-insert into warehouse values (null, 'santo angelo');
-
-insert into manager values (null, 'pedro@example.com', 'pedro', 'pedro');
-
-insert into section values (null, 'FROZEN', 0, 10, 1, 1);
-
-insert into product values (null, 'Sadia', 'FROZEN', 'Carne');
