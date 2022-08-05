ce421e13b68db843e2e237d21d9ae621ad35ffbb
bdonadel <bruna.donadel@mercadolivre.com>
2022-08-05 14:03:13 -0300

    feat: add validation sectionCode positive

--- src/main/java/com/mercadolibre/bootcamp/projeto_integrador/dto/InboundOrderRequestDto.java
+++ src/main/java/com/mercadolibre/bootcamp/projeto_integrador/dto/InboundOrderRequestDto.java
@@ -6,14 +6,16 @@ import lombok.NoArgsConstructor;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 public class InboundOrderRequestDto {
-    @NotNull(message = "O 'sectionCode' não pode estar vazio")
+    @NotNull(message = "O código do setor não pode estar vazio")
+    @Positive(message = "O código do setor deve ser um número positivo")
     private long sectionCode;
 
-    @NotEmpty(message = "A lista de 'batchStock' é obrigatória")
+    @NotEmpty(message = "A lista de lotes é obrigatória")
     private List<@Valid BatchRequestDto> batchStock;
 }
