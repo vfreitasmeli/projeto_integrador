580b78b26a46af024989063be585790bceaf5dab
bdonadel <bruna.donadel@mercadolivre.com>
2022-08-05 11:00:17 -0300

    feat: Add validations

--- src/main/java/com/mercadolibre/bootcamp/projeto_integrador/dto/BatchRequestDto.java
+++ src/main/java/com/mercadolibre/bootcamp/projeto_integrador/dto/BatchRequestDto.java
@@ -2,24 +2,38 @@ package com.mercadolibre.bootcamp.projeto_integrador.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
 public class BatchRequestDto {
+
+    @NotBlank(message = "O Id do produto deve ser informado")
+    @Positive(message = "O Id do produto deve ser um número positivo")
     private long productId;
 
+    @NotBlank(message = "A temperatura atual deve ser informada")
     private float currentTemperature;
 
+    @NotBlank(message = "A temperatura mínima dever ser informada")
     private float minimumTemperature;
 
+    @NotBlank(message = "A quantidade inicial deve ser informada")
     private int initialQuantity;
 
+    @NotBlank(message = "A quantidade atual deve ser informada")
     private int currentQuantity;
 
+    @NotBlank(message = "A data de fabricação deve ser informada")
+    @PastOrPresent(message = "A date de fabricação deve ser menor ou igual a data atual")
     private LocalDate manufacturingDate;
 
+    @NotBlank(message = "A hora de fabricação deve ser informada")
+    @PastOrPresent(message = "A hora de fabricação deve ser menor ou igual a data atual")
     private LocalDateTime manufacturingTime;
 
+    @NotBlank(message = "A data de vencimento deve ser informada")
+    @Future(message = "A data de vencimento deve ser posterior a data atual")
     private LocalDate dueDate;
 }
--- src/main/java/com/mercadolibre/bootcamp/projeto_integrador/dto/InboundOrderRequestDto.java
+++ src/main/java/com/mercadolibre/bootcamp/projeto_integrador/dto/InboundOrderRequestDto.java
@@ -3,11 +3,17 @@ package com.mercadolibre.bootcamp.projeto_integrador.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 public class InboundOrderRequestDto {
+    @NotBlank(message = "O 'sectionCode' não pode estar vazio")
     private long sectionCode;
-    private List<BatchRequestDto> batchStock;
+
+    @NotEmpty(message = "A lista de 'batchStock' é obrigatória")
+    private List<@Valid BatchRequestDto> batchStock;
 }
