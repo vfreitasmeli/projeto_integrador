9d2ad6f505b5962110ef63ee9284862da445b056
felipeticiani-meli <felipe.ticiani@mercadolivre.com>
2022-08-08 16:49:24 -0300

    feat: added invalid format exception handler

--- src/main/java/com/mercadolibre/bootcamp/projeto_integrador/exceptions/CustomExceptionHandler.java
+++ src/main/java/com/mercadolibre/bootcamp/projeto_integrador/exceptions/CustomExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.mercadolibre.bootcamp.projeto_integrador.exceptions;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,13 +18,12 @@ import java.util.stream.Collectors;
 public class CustomExceptionHandler {
 
     /**
-     * Throw unmapped exceptions with HTTP Status 500, printing these exceptions to verify.
+     * Lança exceções não mapeadas com HTTP Status 500, que poderão ser verificadas posterioremente.
      * @throw Exception
      * @param exception
      */
     @ExceptionHandler(Exception.class)
     public Object unmappedExceptionHandler(Exception exception) {
-        System.out.println(exception.toString());
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new CustomError("Internal Server Error",
@@ -42,17 +45,40 @@ public class CustomExceptionHandler {
         return ResponseEntity.status(exception.getStatus()).body(new CustomError(exception));
     }
 
-    // Exceptions for @Valid annotation
+    // Trata as exceções referente às validações (@Valid)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<CustomError> handle(MethodArgumentNotValidException exception) {
-        // Create list with all errors on exception
+    public ResponseEntity<CustomError> invalidFieldsHandler(MethodArgumentNotValidException exception) {
+        // Cria uma lista com todos os erros da exceção recebida.
         List<FieldError> errors = exception.getBindingResult().getFieldErrors();
 
-        // New CustomError instance, transforming each FieldError to String
-        CustomError error = new CustomError("Invalid fields", errors.stream()
+        // Instancia um novo CustomError, transformando cada FieldError em String
+        CustomError error = new CustomError("Campo(s) inválido(s)", errors.stream()
                 .map(FieldError::getDefaultMessage)
                 .distinct()
                 .collect(Collectors.joining(" | ")), LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    // Trata as exceções lançadas quando ocorre erro na transformação do JSON recebido em Objeto Java, formato inválido.
+    @ExceptionHandler({MismatchedInputException.class, InvalidFormatException.class, HttpMessageNotReadableException.class})
+    public ResponseEntity<CustomError> invalidFormatHandler(MismatchedInputException exception) {
+        /*
+        Através do caminho do erro (path), pega o campo (getFieldName) onde o erro ocorreu. Quando o erro ocorrer
+        dentro de uma lista, o replace substituirá o valor "null" por ":" para melhorar a visualização.
+         */
+        String field = exception.getPath().stream()
+                .map(JsonMappingException.Reference::getFieldName)
+                .collect(Collectors.joining())
+                .replace("null", ":");
+
+        // Concatena uma mensagem contendo o nome do campo e o tipo esperado no campo.
+        String message = field + ": esperado " + exception.getTargetType().getSimpleName() + ".";
+
+        /*
+        Instancia um CustomError, recebendo o simpleName da exception no primeiro parâmetro
+        e a mensagem no segundo parâmetro.
+         */
+        CustomError error = new CustomError("Campo(s) inválido(s)", message, LocalDateTime.now());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
